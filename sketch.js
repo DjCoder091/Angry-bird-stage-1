3fbc54bca418acbe5fa09f03844ea6a0c438d725
DjCoder091 <71996904+DjCoder091@users.noreply.github.com>
2020-10-16 14:43:43 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+const Engine=Matter.Engine
+const World=Matter.World
+const Bodies=Matter.Bodies
+
+var engine,world,object
+
+var box1,box2,box3,box4,box5
+
+var ground
+
+var Pig1,Pig2
+
+var Log1,Log2,Log3,Log4
+var bird1
+function setup() {
+  createCanvas(1200,400);
+engine=Engine.create()
+  world=engine.world
+  ground=new Ground(600,height,1200,20)
+box1=new Box(700,320,70,70)
+box2=new Box(920,320,70,70)
+  Pig1=new Pig(810,350)
+Log1=new Logs(810,260,300,PI/2)
+
+box3=new Box(700,240,70,70)
+box4=new Box(920,240,70,70)
+  Pig2=new Pig(810,220)
+Log2=new Logs(810,180,300,PI/2)
+box5=new Box(810,160,70,70)
+Log3=new Logs(760,120,150,PI/7)
+Log4=new Logs(870,120,150,PI/7)
+bird1=new Bird(100,100)
+}
+
+function draw() {
+  background(255,255,255);
+  Engine.update(engine)  
+  box1.display()
+  box2.display()
+  ground.display()
+  Pig1.display()
+  Log1.display()
+  Pig2.display()
+  box3.display()
+  box4.display()
+  Log2.display()
+  Log3.display()
+  box5.display()
+  Log4.display()
+  bird1.display()
+
+}
